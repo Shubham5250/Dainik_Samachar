d650e0a48027495ff13a1362941d99dadcad7fa6
Shubham5250 <shubhampaw312@gmail.com>
2023-06-01 02:25:55 +0530

    search view added

--- app/src/main/java/com/example/dainik_samachar/MainActivity.java
+++ app/src/main/java/com/example/dainik_samachar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dainik_samachar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,23 +9,58 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.dainik_samachar.Models.NewsApiResponse;
 import com.example.dainik_samachar.Models.NewsHeadlines;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SelectListener{
+public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener{
 
     RecyclerView recyclerView;
     CustomAdapter adapter;
     ProgressDialog progressDialog;
+    Button b1, b2, b3, b4, b5, b6, b7;
+    SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchView = findViewById(R.id.searchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                RequestManager manager = new RequestManager(MainActivity.this);
+                progressDialog.show();
+                manager.getNewsHeadlines(listener,"general",query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        b1 = findViewById(R.id.btn1);
+        b1.setOnClickListener(this);
+        b2 = findViewById(R.id.btn2);
+        b2.setOnClickListener(this);
+        b3 = findViewById(R.id.btn3);
+        b3.setOnClickListener(this);
+        b4 = findViewById(R.id.btn4);
+        b4.setOnClickListener(this);
+        b5 = findViewById(R.id.btn5);
+        b5.setOnClickListener(this);
+        b6 = findViewById(R.id.btn6);
+        b6.setOnClickListener(this);
+        b7 = findViewById(R.id.btn7);
+        b7.setOnClickListener(this);
         progressDialog = new ProgressDialog(this);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         progressDialog.setIndeterminate(true);
@@ -36,8 +72,14 @@ public class MainActivity extends AppCompatActivity implements SelectListener{
     private final OnDataFetchListener<NewsApiResponse> listener = new OnDataFetchListener<NewsApiResponse>() {
         @Override
         public void onFetchData(List<NewsHeadlines> list, String message) {
-            showNews(list);
-            progressDialog.dismiss();
+            if(list.isEmpty()){
+                Toast.makeText(MainActivity.this, "No data found", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+            else{
+                showNews(list);
+                progressDialog.dismiss();
+            }
         }
 
         @Override
@@ -59,5 +101,14 @@ public class MainActivity extends AppCompatActivity implements SelectListener{
         startActivity(new Intent(MainActivity.this, DetailsActivity.class)
                 .putExtra("data", headlines));
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String category = button.getText().toString();
+        RequestManager manager = new RequestManager(this);
+        progressDialog.show();
+        manager.getNewsHeadlines(listener,category,null);
     }
 }
